4578028f31e8d019f4835364be5c168c32683cff
MirrorBottle <setiawanbayu66152@gmail.com>
2022-02-06 03:12:26 +0800

    chore: make new branch

--- src/api/note.js
+++
@@ -1,9 +0,0 @@
-import Resource from '@/api/resource';
-
-class NoteResource extends Resource {
-  constructor() {
-    super('notes');
-  }
-}
-
-export { NoteResource as default };
--- src/api/todo.js
+++
@@ -1,9 +0,0 @@
-import Resource from '@/api/resource';
-
-class TodoResource extends Resource {
-  constructor() {
-    super('todos');
-  }
-}
-
-export { TodoResource as default };
